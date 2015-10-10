5f2cbb864de70a39153ad411299562fcc9cc72d0
Zapk <zapgravity@gmail.com>
2015-10-10 12:37:58 -0700

    Fixed loading

--- lua/baseplate/init.lua
+++ lua/baseplate/init.lua
@@ -1,4 +1,4 @@
-require( 'baseplates.console' )  -- Allows typing ".myCode" in the Blockland console to run "myCode" as Lua.
-require( 'baseplates.clients' )  -- Library for handling Blockland clients with Lua.
-require( 'baseplates.vector' )   -- Library for vectors that are easier to work with than Torque's.
-require( 'baseplates.timer' )    -- Timer library that hooks into Torque schedules.
+require( 'baseplate.console' )  -- Allows typing ".myCode" in the Blockland console to run "myCode" as Lua.
+require( 'baseplate.clients' )  -- Library for handling Blockland clients with Lua.
+require( 'baseplate.vector' )   -- Library for vectors that are easier to work with than Torque's.
+require( 'baseplate.timer' )    -- Timer library that hooks into Torque schedules.
